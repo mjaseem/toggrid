ad5adf86e66c06d88af809d4ea8ec6853be87748
jaseem <jaseemumar@gmail.com>
2019-07-15 10:44:30 +0530

    Go back to immersive mode when gaining focus

--- app/src/main/java/com/mjaseem/toggrid/FullscreenActivity.java
+++ app/src/main/java/com/mjaseem/toggrid/FullscreenActivity.java
@@ -49,13 +49,17 @@ public class FullscreenActivity extends AppCompatActivity {
         webView.addJavascriptInterface(new Config(), "Config");
         webView.addJavascriptInterface(new Cache(), "Cache");
 
+        hideSystemUi();
+
+    }
+
+    private void hideSystemUi() {
         webView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
 //                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE //Uncomment to hide action bar
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
     }
 
 
@@ -93,6 +97,14 @@ public class FullscreenActivity extends AppCompatActivity {
 
     private void loadView() {
         webView.loadUrl(URL);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUi();
+        }
     }
 }
 
